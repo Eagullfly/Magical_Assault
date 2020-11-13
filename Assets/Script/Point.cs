955bee0fcf14294a71de9ea910320f1f69d7d209
Ryan Wright <rwright3702@sdsu.edu>
2020-11-13 15:09:21 -0800

    Initial commit

---
+++ Assets/Script/Point.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public struct Point
+{
+    public int x { get; set; }
+
+    public int Y { get; set; }
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.Y = y;
+    }
+}
